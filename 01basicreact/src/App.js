be5600a008e7d3cb20ff74531b7f3541c385d874
nishant <nishantkudale04@gmail.com>
2025-07-18 12:44:17 +0530

    Initialize Vite React project with essential configurations and files
    - Created package.json with dependencies and scripts for development, build, and linting.
    - Added Vite logo SVG to the public directory.
    - Implemented main application component (App.jsx) with a simple heading.
    - Set up entry point (main.jsx) for rendering the App component using React's StrictMode.
    - Configured Vite with React plugin in vite.config.js.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>learning react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+    <h1>learning react with vite | Nishant Kudale</h1>
+  )
+}
+
+export default App
