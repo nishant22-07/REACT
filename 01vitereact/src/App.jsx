28c9ee3da913d7be9fe1f6be27a083ed170f96a6
nishant <nishantkudale04@gmail.com>
2025-12-02 18:49:59 +0530

    Add username display in App component and update main rendering logic

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,11 @@ import Chai from "./chai"
 
 
 function App() {
+  const username = " Nishant";
   return (
     <>
     <Chai/>
+    <h1>chai aur code{username}</h1>
     <p>this is our project</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom react! </h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type : 'a',
+//     props : {
+//         href : 'https://google.com',
+//         target: '_blank'
+//     },
+//     Children : "Click me to visit Google"
+
+// }
+
+const anotherUser = " Chai "
+
+const reactElement  = React.createElement(
+    'a',
+    {href: 'https://google.com' ,target:'_blank'},
+    'Click me to visit google',
+    anotherUser
+)
+
+
+const anotherElement = (
+    <a href="https.//google.com" target='_blank'>Visit Google</a>
+)
 
 createRoot(document.getElementById('root')).
 render(
-  
-    <App />
+    
+    reactElement
+    
   
 )
