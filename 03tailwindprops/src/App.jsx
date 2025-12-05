1167dae361670b75b83cb146cee02e4cfabc69a1
nishant <nishantkudale04@gmail.com>
2025-12-06 00:46:23 +0530

    feat: simplify App component by removing unused code and retaining Card components

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,36 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let myObj  = {
+    username : "Nishant",
+    age : 21
+  }
+  let arr = [1,2,3,4];
 
   return (
     <>
-          <h1 className='bg-green-400 text-black p-2 rounded-2xl' >Tailwind css test</h1>
-          <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-  <div class="md:flex">
-    <div class="md:shrink-0">
-      <img
-        class="h-48 w-full object-cover md:h-full md:w-48 border-4 p-2 rounded-2xl"
-        src="https://images.pexels.com/photos/31136767/pexels-photo-31136767.jpeg"
-        alt="Modern building architecture"
-      />
-    </div>
-    <div class="p-8">
-      <div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">Company retreats</div>
-      <a href="#" class="mt-1 block text-lg leading-tight font-medium text-black hover:underline">
-        Incredible accommodation for your team
-      </a>
-      <p class="mt-2 text-gray-500">
-        Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of
-        places to do just that.
-      </p>
-    </div>
-  </div>
-</div>   
+      <h1 className='bg-green-400 text-black p-2 rounded-2xl' >Tailwind test</h1>
+
+      <Card username="Chai aur code" btnText = "click me" />
+      <Card username="Christina" />
+      
+   
     </>
   )
 }
 
 export default App
+
