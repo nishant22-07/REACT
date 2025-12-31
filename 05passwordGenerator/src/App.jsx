52baad192df1ac27c6f6bbadd664772ee25dc715
nishant <nishantkudale04@gmail.com>
2025-12-31 15:34:32 +0530

    feat: implement password generation logic and UI components in App

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,21 +1,128 @@
-import { useState } from 'react'
+import { useState , useCallback ,useEffect , useRef} from 'react'
 
 import './App.css'
 
 function App() {
   const [length, setLength] = useState(8);
-
   const[numberAllowed , setNumberAllowed] = useState(false);
-
   const[charAllowed , setCharAllowed] = useState(false);
+  const[password , setPassword] = useState("");
 
-  const[password , setPassword] = useState()
+  const passwordGenerator = useCallback(() => {
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let pass = "";
+    if(numberAllowed) str+= "0123456789";
+    if(charAllowed) str+="!@#$%^&*-_=+[]{}`~";
+
+    for (let i = 1; i <= length; i++) {
+
+      let char = Math.floor(Math.random() * str.length +1);
+      pass+= str.charAt(char);
+    }
+    setPassword(pass);
+
+  },[length, numberAllowed,charAllowed,setPassword])
+
+
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  const copyPasswordToClipboard = useCallback (() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0,8)
+    window.navigator.clipboard.writeText(password)
+  })
+
+  const onMouseOver = (e)=>{
+    e.target.style.backgroundColor = "purple"
+
+  }
+  
+  const onMouseOut = (e) =>{
+    e.target.style.backgroundColor = "blue"
+  }
+
+  useEffect(() => {
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+
+
+  
+
+
+
+  
 
 
 
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md
+       rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+        <h1 className= 'text-white text-center'>Password Generator</h1>
+        <div className='flex bg-white shadow rounded-lg 
+          overflow-hidden mb-4 '>
+          <input type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3 '
+          placeholder='password'
+          readOnly
+          ref={passwordRef}
+
+           />
+
+           <button className=' outline-none bg-blue-800
+            text-white px-3 py=0.5 shrink-0'
+            onClick={copyPasswordToClipboard}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}> copy </button>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          
+          <input 
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className='cursor-pointer'
+            onChange={(e => {setLength(e.target.value)})}
+           />
+           <label > Length:{length}</label>
+
+        <div className='flex items-center gap-x-1'>
+          
+          <input type='checkbox'
+            defaultChecked={charAllowed}
+            id='characterInput'
+            onChange={()=>
+              setCharAllowed((prev) => !prev)
+            }
+          />
+          <label htmlFor='characterInput'> Characters </label>
+        </div>
+        
+
+
+        <div className='flex items-center gap-x-1'>
+          
+          <input type='checkbox'
+            defaultChecked={numberAllowed}
+            id ="numberInput"
+            onChange={()=>{
+              setNumberAllowed((prev => ! prev));
+            }}
+          />
+          <label htmlFor='numberInput'>Numbers </label>
+        </div>
+        
+           
+
+        </div>
+       </div>
+
+
+      
+
     </>
   )
 }
