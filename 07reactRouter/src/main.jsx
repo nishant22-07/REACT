56c818bb14c3a30c6a0973c11fe19a72f5106fe1
nishant <nishantkudale04@gmail.com>
2026-01-05 22:48:25 +0530

    feat: implement routing with Layout, Home, About, and Contact components

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"About",
+        element: <About/>,
+
+      },
+      {
+        path:"Contact",
+        element: <Contact/>,
+
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
